acf784e03ae856c09ea4731d9f6e75e8b201489a
alex harley <alexbruceharley@gmail.com>
2016-03-11 22:29:25 +1300

    added message parser

---
+++ message/MessageParser.py
@@ -0,0 +1,21 @@
+import Constants
+
+
+def parse(message):
+    parsed_messages = []
+    raw_messages = message.split(Constants.MESSAGE_SEPARATOR)
+    for raw in raw_messages:
+        parsed_messages.append(_parse_message(raw))
+
+    return parsed_messages
+
+
+def _parse_message(message):
+    parts = message.split(Constants.MESSAGE_PART_SEPARATOR)
+    message_object = {}
+
+    message_object.topic = parts[0]
+    message_object.action = parts[1]
+    message_object.data = parts[2:]
+
+    return message_object
