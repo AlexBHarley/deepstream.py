0ebaeab9ebf4237051e00a034e7ed8a6c3f4aa65
alex harley <alexbruceharley@gmail.com>
2016-03-25 14:21:38 +1300

    further changes to test_server

--- tests/feature_tests/connecting_tests.py
+++ tests/feature_tests/connecting_tests.py
@@ -1,37 +1,47 @@
-from tests.feature_tests.test_server import ServerThread, TestServer
+from tests.feature_tests.test_server import TestServer
 from src.DeepStreamClient import DeepStreamClient
 from contexts import setup, assertion, teardown
 import unittest
-
+import threading
+import time
 
 class WhenConnectingAClient:
 
     def __init__(self):
-        self.server_thread = None
+        self.server = TestServer('127.0.0.1', 5460)
 
     @setup
     def given_a_test_server(self):
-        self.server_thread = ServerThread('127.0.0.1', 5555)
-        self.server_thread.start()
+        self.server_thread = threading.Thread(target=self.server.start_listening)
+        self.server_thread.daemon = True
+        try:
+            self.server_thread.start()
+        except Exception:
+            pass
 
     @assertion
     def test_server_is_ready(self):
-        assert self.server_thread.server.is_ready == True
+        assert self.server.is_ready == True
     '''
     @assertion
     def server_has_no_active_connections(self):
         assert (len(self.server_thread.server.connections) == 0)
     '''
-    @teardown
-    def teardown(self):
-        self.server_thread.stop()
-'''
+
+
     @assertion
     def client_is_initialised(self):
-        self.client = DeepStreamClient('127.0.0.1', 6022)
+        self.client = DeepStreamClient('127.0.0.1', 5460)
+        assert (self.client != None)
 
-        self.assertTrue(self.client != None)
-
+    @teardown
+    def teardown(self):
+        self.server.stop()
+        try:
+            self.server_thread.join(1)
+        except Exception:
+            pass
+'''
     @assertion
     def server_has_an_active_connection(self):
         assert len(self.server.connections) == 1
--- tests/feature_tests/test_server.py
+++ tests/feature_tests/test_server.py
@@ -1,54 +1,35 @@
 import socket
 import threading
-import asyncore
 
 
-class TestServer(asyncore.dispatcher):
+class TestServer:
 
     def __init__(self, ip, port):
-        asyncore.dispatcher.__init__(self)
         self.is_ready = True
         self.last_socket = None
+        self.running = False
+        self.connection_count = 0
         self.connections = []
         self.all_messages = []
         self.last_message = b''
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.settimeout(5)
-        self.set_reuse_addr()
-        self.bind((ip, port))
-        self.listen(5)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((ip, port))
 
-    def handle_accept(self):
-        pair = self.accept()
-        if pair is not None:
-            sock, addr = pair
-            try:
-                data = sock.recv(1024)
-                self.last_message = data
-                self.all_messages += data
-                self.connections += sock
-            except:
-                pass
+    def handle_client(self, client):
+        data = client.recv(1024)
+        self.last_message = data
+        print(data)
+        client.close()
 
-
-class ServerThread(threading.Thread):
-    def __init__(self, ip, port):
-        threading.Thread.__init__(self)
-        self.server = TestServer(ip, port)
-
-    def run(self):
-        print('Starting run')
-        asyncore.loop()
+    def start_listening(self):
+        self.sock.listen(5)
+        self.running = True
+        while self.running:
+            client, addr = self.sock.accept()
+            self.connection_count += 1
+            print('Handling client ' + str(client))
+            self.handle_client(client)
 
     def stop(self):
-        print('Stopping')
-        self.server.close()
-        self.join()
-
-'''
-t = ServerThread('127.0.0.1', 5555)
-t.start()
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 5555))
-s.send(b'here i am')
-t.stop()'''
+        self.running = False
+        self.sock.close()
