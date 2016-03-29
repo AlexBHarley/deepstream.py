2f9496caf3f92723d11ff046c3e6c796b66c7bb8
alex harley <alexbruceharley@gmail.com>
2016-03-30 00:12:58 +1300

    for travis

--- src/main.py
+++ src/main.py
@@ -1,12 +1,22 @@
 from src.DeepStreamClient import DeepStreamClient
+from tests.feature_tests.test_server import TestServer
+from src import Constants as C
+import threading
+import time
 
-username = "XXX"
-password = "YYY"
+def show(data):
+    print(data)
 
-ds = DeepStreamClient("127.0.0.1", 6021)
-ds.login(username, password)
 
-def print_data_received(message):
-    print("-----------------")
-    print(message)
-ds.event.subscribe('first_event', print_data_received)
+server = TestServer("127.0.0.1", 6767)
+server_thread = threading.Thread(target=server.start_listening)
+server_thread.setDaemon(True)
+server_thread.start()
+
+client = DeepStreamClient("127.0.0.1", 6767)
+credentials = {}
+credentials["username"] = "user_too_many_auth"
+credentials["password"] = "pass_too_many_auth"
+client.login(credentials, show)
+time.sleep(1)
+assert client.get_connection_state() == C.CONNECTION_STATE_AUTHENTICATING
