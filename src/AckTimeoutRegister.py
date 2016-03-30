da9c6aa62fbf0d469b7dafc9c5e9287a024967fc
alex harley <alexbruceharley@gmail.com>
2016-03-30 23:16:08 +1300

    added AckTimeoutRegister

---
+++ src/AckTimeoutRegister.py
@@ -0,0 +1,40 @@
+from pyee import EventEmitter
+from threading import Timer
+from src import Constants as C
+
+class AckTimeoutRegister:
+
+    def __init__(self, client, topic, timeout_duration):
+        self._client = client
+        self._topic = topic
+        self._timeout_duration = timeout_duration
+        self._register = {}
+        self._emitter = EventEmitter()
+
+    def add(self, name, action):
+        """
+        Adds an action to the registry with a timeout. If the timeout happens
+        the register emits an error.
+
+        :param name: Name of the occurrence to add to the registry
+        :param action: Type of action it is (event etc)
+        """
+        unique_name = action + "-" + name if action is not None else name
+        if unique_name in self._register:
+            self.clear({"data": [unique_name]})
+        self._register[unique_name] = Timer(self._timeout_duration, self._on_timeout, args=(unique_name,))
+        self._register[unique_name].start()
+
+    def clear(self, event_data):
+        timeout = self._register[event_data]
+        if timeout is not None:
+            timeout.cancel()
+        else:
+            self._client._on_error(self._topic, C.EVENT_UNSOLICITED_MESSAGE, event_data)
+
+    def _on_timeout(self, event_name):
+        self._register.pop(event_name)
+        msg = 'No ACK message received in time for ' + event_name
+        self._client._on_error(self._topic, C.EVENT_ACK_TIMEOUT, msg)
+        self._emitter.emit('timeout', event_name)
+
--- src/EventHandler.py
+++ src/EventHandler.py
@@ -1,6 +1,6 @@
-from src import Constants
+from src import Constants as C
 from pyee import EventEmitter
-
+from src.AckTimeoutRegister import AckTimeoutRegister
 
 class EventHandler:
 
@@ -8,17 +8,20 @@ class EventHandler:
         self._connection = connection
         self._emitter = EventEmitter()
         self._client = client
+        self._ack_timeout_register = AckTimeoutRegister(self._client, C.TOPIC_EVENT, 5)
 
     def handle(self, message):
-        name = message['data'][1]
-        if len(message['data']) == 2:
-            self._emitter.emit(name, name)
+        if len(message['data']) == 2: #ack
+            name = message['data'][1]
+            self._emitter.emit(name)
         else:
             print('passed, data too short')
             pass
 
     def subscribe(self, event_name, callback):
+        self._ack_timeout_register.add(event_name, C.ACTIONS_SUBSCRIBE)
         self._emitter.on(event_name, callback)
-        self._connection.send_message(Constants.TOPIC_EVENT, Constants.ACTIONS_SUBSCRIBE, [event_name])
+        print('here')
+        self._connection.send_message(C.TOPIC_EVENT, C.ACTIONS_SUBSCRIBE, [event_name])
 
 
